4b668cb21e9abdf43e8a01c380d1c94e86d82670
ebrahimkhodadadi <ebrahimkhodadadideveloper@gmail.com>
2022-11-25 02:54:29 +0330

    Add Example to MongoDBAspNetCore

---
+++ Data.Mongo/Collections/Log.cs
@@ -0,0 +1,21 @@
+﻿using Data.Mongo.Attributes;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace Data.Mongo.Collections;
+
+[BsonCollection("Log")]
+[BsonIgnoreExtraElements]
+public class Log : MongoBaseDocument
+{
+    public LogType LogType { get; set; }
+    public string Code { get; set; }
+    public int ClientID { get; set; }
+    public DateTime Created { get; set; }
+}
+
+public class LogType
+{
+    public int key { get; set; }
+    public string value { get; set; }
+}
--- MongoDBAspNetCore/Program.cs
+++ MongoDBAspNetCore/Program.cs
@@ -1,9 +1,19 @@
+using Data.Mongo;
+using Data.Mongo.Collections;
+using Data.Mongo.Repo;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMongo(builder.Configuration);
 
 var app = builder.Build();
+
+app.MapGet("/GetAllLogs", (IMongoRepository<Log> db) =>
+    {
+        return Results.Ok(db.GetAll());
+    });
 
 if (app.Environment.IsDevelopment())
 {
